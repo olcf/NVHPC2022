f7fc00cd51e5ef3575745efe02ca6590a4189cd6
Matt S <mstack@nvidia.com>
2022-01-11 18:29:46 -0800

    adding batch script

---
+++ stdpar/batch.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#SBATCH -A ntrain4_g
+#SBATCH -C gpu
+#SBATCH -G 1
+#SBATCH -q regular
+#SBATCH -t 5
+#SBATCH -n 1
+#SBATCH -c 128
+#SBATCH --ntasks-per-node=1
+#SBATCH --gpus-per-task=1
+
+srun ./a.out
